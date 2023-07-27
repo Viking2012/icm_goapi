e8051519370be5eb5ac173cf6aa7f316d1271713
AJO <Viking2012@gmail.com>
2023-07-27 14:05:33 -0400

    refactor: follow better golang filename conventions

--- orm/bankMaster.go
+++ orm/bankMaster.go
@@ -21,26 +21,26 @@ type BankMaster struct {
 	CreationDate time.Time `json:"Bank Record Creation Date"`
 
 	// Flags
-	FlagAlternativePayee                                        bool         `json:"Flag: AlternativePayee"`
-	FlagAlternativePayeeRotated                                 bool         `json:"Flag: Alternative Rotated"`
-	FlagAlternativePayeesMultiple                               bool         `json:"Flag: Multiple Alternative Payees"`
-	DescriptiveAlternativePayeeNames                            string       `json:"Descriptive: Alternative Payee Names"`
-	FlagBankUsedForSingleDocType                                bool         `json:"Flag: Bank Used for Single Doc Type"`
-	FlagBlockedVendorBankAccountPaid                            bool         `json:"Flag: Blocked Vendor Bank Account Paid"`
-	FlagCountOfSecondaryBankVendors                             IntFromFloat `json:"Flag: Count of Secondary Bank Vendors"`
-	DescriptiveSecondaryBankVendors                             string       `json:"Secondary Bank Vendors"`
-	FlagDisappearingBankAccountFiOnlyInvoicesPaid               bool         `json:"Flag: Disappearing Bank Account; FI Only Invoices Paid"`
-	FlagDisappearingBankAccountFiOnlyInvoicesPaidLastMonth      bool         `json:"Flag: Disappearing Bank Account; FI Only Invoices Paid Last Month"`
-	FlagDisappearingBankAccountOnlyLimitPoInvoicesPaid          bool         `json:"Flag: Disappearing Bank Account; Only Limit PO Invoices Paid"`
-	FlagDisappearingBankAccountOnlyLimitPoInvoicesPaidLastMonth bool         `json:"Flag: Disappearing Bank Account; Only Limit PO Invoices Paid Last Month"`
-	FlagNotTheLargestBank                                       bool         `json:"Flag: Not the Largest Bank"`
-	FlagPaidToDisappearingAndReappearingBankAccount             bool         `json:"Flag: Paid To Disappearing and Reappearing Bank Account"`
-	FlagRiskyPaymentsToDisappearingBankAccount                  bool         `json:"Flag: Risky Payments to Disappearing Bank Account"`
-	FlagRiskyPaymentsToDisappearingBankAccountThisMonth         bool         `json:"Flag: Risky Payments to Disappearing Bank Account This Month"`
-	FlagTravelingBankAccount                                    bool         `json:"Flag: Traveling Bank Account"`
-	FlagTop5BenfordDeviation                                    bool         `json:"Flag: Top5BenfordDeviation"`
-	DescriptiveBenford                                          string       `json:"BenfordDescriptive"`
-	FlagVendorClusterCount                                      IntFromFloat `json:"Flag: Vendor Cluster Count"`
+	FlagAlternativePayee                                        bool   `json:"Flag: AlternativePayee"`
+	FlagAlternativePayeeRotated                                 bool   `json:"Flag: Alternative Rotated"`
+	FlagAlternativePayeesMultiple                               bool   `json:"Flag: Multiple Alternative Payees"`
+	DescriptiveAlternativePayeeNames                            string `json:"Descriptive: Alternative Payee Names"`
+	FlagBankUsedForSingleDocType                                bool   `json:"Flag: Bank Used for Single Doc Type"`
+	FlagBlockedVendorBankAccountPaid                            bool   `json:"Flag: Blocked Vendor Bank Account Paid"`
+	FlagCountOfSecondaryBankVendors                             int32  `json:"Flag: Count of Secondary Bank Vendors"`
+	DescriptiveSecondaryBankVendors                             string `json:"Secondary Bank Vendors"`
+	FlagDisappearingBankAccountFiOnlyInvoicesPaid               bool   `json:"Flag: Disappearing Bank Account; FI Only Invoices Paid"`
+	FlagDisappearingBankAccountFiOnlyInvoicesPaidLastMonth      bool   `json:"Flag: Disappearing Bank Account; FI Only Invoices Paid Last Month"`
+	FlagDisappearingBankAccountOnlyLimitPoInvoicesPaid          bool   `json:"Flag: Disappearing Bank Account; Only Limit PO Invoices Paid"`
+	FlagDisappearingBankAccountOnlyLimitPoInvoicesPaidLastMonth bool   `json:"Flag: Disappearing Bank Account; Only Limit PO Invoices Paid Last Month"`
+	FlagNotTheLargestBank                                       bool   `json:"Flag: Not the Largest Bank"`
+	FlagPaidToDisappearingAndReappearingBankAccount             bool   `json:"Flag: Paid To Disappearing and Reappearing Bank Account"`
+	FlagRiskyPaymentsToDisappearingBankAccount                  bool   `json:"Flag: Risky Payments to Disappearing Bank Account"`
+	FlagRiskyPaymentsToDisappearingBankAccountThisMonth         bool   `json:"Flag: Risky Payments to Disappearing Bank Account This Month"`
+	FlagTravelingBankAccount                                    bool   `json:"Flag: Traveling Bank Account"`
+	FlagTop5BenfordDeviation                                    bool   `json:"Flag: Top5BenfordDeviation"`
+	DescriptiveBenford                                          string `json:"BenfordDescriptive"`
+	FlagVendorClusterCount                                      int32  `json:"Flag: Vendor Cluster Count"`
 
 	// Transactional summary
 	AmountTotalValueUSD float64 `json:"Sum_REGUP_WRBTR_USD"`
