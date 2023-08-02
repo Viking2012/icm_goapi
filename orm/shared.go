1caae355a0e82392d131c218ee326cca733b4974
AJO <Viking2012@gmail.com>
2023-08-02 10:17:48 -0400

    bugfix: re-add BoolFromFloat and IntFromFloat types to read boolean values which are stored as REALs in snowflake

--- orm/shared.go
+++ orm/shared.go
@@ -1,7 +1,11 @@
 package orm
 
 import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
 	"reflect"
+	"strconv"
 )
 
 // ICMEntity is an interface used by the sql encoder for any
@@ -9,6 +13,7 @@ import (
 // TODO(ajo): consider changing this interface to an ICMObject
 type ICMEntity interface {
 	GetFlags() ICMEntity
+	//LoadFromRow(row *sql.Rows) error
 }
 
 func GetFields(a ICMEntity) []string {
@@ -34,4 +39,83 @@ func NewModel(a ICMEntity) map[string]int16 {
 		model[key] = 0
 	}
 	return model
+}
+
+// BoolFromFloat from https://stackoverflow.com/a/37214476
+type BoolFromFloat bool
+
+func (bit *BoolFromFloat) UnmarshalJSON(data []byte) error {
+	var scan BoolFromFloat
+	f, err := strconv.ParseFloat(string(data), 64)
+	if err != nil {
+		return errors.New(fmt.Sprintf("incompatible float value for a BoolFromFloat UnarshalJSON: %s", data))
+	}
+	s := fmt.Sprintf("%.0f", f)
+	switch s {
+	case "0":
+		scan = false
+	case "1":
+		scan = true
+	default:
+		return errors.New(fmt.Sprintf("incompatible float value for a BoolFromFloat UnarshalJSON: %s", s))
+	}
+	*bit = scan
+	return nil
+}
+
+func (bit *BoolFromFloat) Scan(src interface{}) error {
+	var scan BoolFromFloat
+	f, ok := src.(float64)
+	if !ok {
+		return errors.New(fmt.Sprintf("incompatible float value for a BoolFromFloat Scan Float: %s", src))
+	}
+	s := fmt.Sprintf("%.0f", f)
+	switch s {
+	case "0":
+		scan = false
+	case "1":
+		scan = true
+	default:
+		return errors.New(fmt.Sprintf("incompatible float value for a BoolFromFloat Scan: %s", src))
+	}
+	*bit = scan
+	return nil
+}
+
+func (bit *BoolFromFloat) Value() (driver.Value, error) {
+	if *bit {
+		return driver.Value(float64(1)), nil
+	} else {
+		return driver.Value(float64(0)), nil
+	}
+}
+
+type IntFromFloat int64
+
+func (ci *IntFromFloat) Scan(src interface{}) error {
+	s := fmt.Sprintf("%.0f", src)
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return err
+	}
+	*ci = IntFromFloat(int64(i))
+	return err
+}
+
+func (ci *IntFromFloat) Value() (driver.Value, error) {
+	return driver.Value(float64(*ci)), nil
+}
+
+func (ci *IntFromFloat) UnmarshalJSON(data []byte) error {
+	f, err := strconv.ParseFloat(string(data), 64)
+	if err != nil {
+		return err
+	}
+	s := fmt.Sprintf("%.0f", f)
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return err
+	}
+	*ci = IntFromFloat(int64(i))
+	return err
 }
